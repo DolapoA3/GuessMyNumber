1d9d1b1a186257afc7106c3014c33f4f3df7e8f7
LeoKing33 <lapo8387@gmail.com>
2022-10-06 23:16:14 -0500

    Setting Dynamic Widths with Dimensions API

--- components/ui/Card.js
+++ components/ui/Card.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Dimensions } from 'react-native';
 
 import Colors from '../../constants/colors';
 
@@ -8,13 +8,15 @@ function Card({ children }) {
 
 export default Card;
 
+const deviceWidth = Dimensions.get('window').width;
+
 const styles = StyleSheet.create({
   card: {
     justifyContent: 'center',
     alignItems: 'center',
     padding: 16,
     marginHorizontal: 24,
-    marginTop: 36,
+    marginTop: deviceWidth < 380 ? 18 : 36,
     backgroundColor: Colors.primary4,
     borderRadius: 8,
     elevation: 4,
